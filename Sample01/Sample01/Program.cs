879f5dbf4d8a0e928238b1b0b591b5d74910093e
Maxim V <vrffmax@yandex.ru>
2024-04-29 12:46:48 +0300

    Init

---
+++ Sample01/Sample01/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Sample01;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello. This sample # 01.");
+
+        var random = new Random();
+
+        var angle = random.NextDouble();
+
+        var byteValue = (byte)(byte.MaxValue * angle);
+        var shortValue = (short)(short.MaxValue * angle);
+        var intValue = (int)(int.MaxValue * angle);
+
+        Console.WriteLine("{0}\t{1}\t{2}\t{3}", angle, byteValue, shortValue, intValue);
+
+        Console.ReadKey(intercept: true);
+
+        //
+
+        double byteCounter = 0;
+        var draftByteCounter = 0;
+
+        byte draftByteValue;
+
+        do
+        {
+            if (random.NextDouble() <= angle)
+            {
+                draftByteCounter++;
+            }
+
+            var draftAngle = draftByteCounter / ++byteCounter;
+
+            draftByteValue = (byte)(byte.MaxValue * draftAngle);
+
+            Console.WriteLine("{0}\t{1}\t{2}", byteCounter, byteValue, draftByteValue);
+        }
+        while (draftByteValue != byteValue);
+
+        Console.ReadKey(intercept: true);
+
+        //
+
+        double shortCounter = 0;
+        var draftShortCounter = 0;
+
+        short draftShortValue;
+
+        do
+        {
+            if (random.NextDouble() <= angle)
+            {
+                draftShortCounter++;
+            }
+
+            var draftAngle = draftShortCounter / ++shortCounter;
+
+            draftShortValue = (short)(short.MaxValue * draftAngle);
+
+            Console.WriteLine("{0}\t{1}\t{2}", shortCounter, shortValue, draftShortValue);
+        }
+        while (draftShortValue != shortValue);
+
+        Console.ReadKey(intercept: true);
+
+        //
+
+        const int probabilityQuality = 3;
+
+        int intCounter = 0;
+        var draftIntCounter = new int[probabilityQuality];
+        int draftIntValue;
+
+        do
+        {
+            for (var index = 0; index < probabilityQuality; index++)
+            {
+                if (random.NextDouble() <= angle)
+                {
+                    draftIntCounter[index]++;
+                }
+            }
+
+            var draftAngle = (double)draftIntCounter.Sum() / (draftIntCounter.Length * ++intCounter);
+
+            draftIntValue = (int)(int.MaxValue * draftAngle);
+
+            Console.WriteLine("{0}\t{1}\t{2}", intCounter, intValue, draftIntValue);
+        }
+        while (draftIntValue != intValue);
+
+        Console.ReadKey(intercept: true);
+    }
+}
---
+++ Sample02/Sample02/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Sample02;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello. This sample # 01.");
+
+        var random = new Random();
+
+        var angle = random.NextDouble();
+
+        var intValue = (int)(int.MaxValue * angle);
+
+        Console.WriteLine("{0}\t{1}", angle, intValue);
+
+        Console.ReadKey(intercept: true);
+
+        //
+
+        const int vectorsLength = 10;
+        const int measuresLength = 10;
+
+        var intCounter = 0;
+        var draftIntCounters = new int[vectorsLength, measuresLength];
+
+        int draftIntValue;
+
+        do
+        {
+            for (var vectorIndex = 0; vectorIndex < vectorsLength; vectorIndex++)
+            {
+                for (var measureIndex = 0; measureIndex < measuresLength; measureIndex++)
+                {
+                    if (random.NextDouble() <= angle)
+                    {
+                        draftIntCounters[vectorIndex, measureIndex]++;
+                    }
+                }
+            }
+
+            intCounter += measuresLength;
+
+            var draftAngle = 0d;
+            for (var vectorIndex = 0; vectorIndex < vectorsLength; vectorIndex++)
+            {
+                var measuresSum = 0;
+                for (var measureIndex = 0; measureIndex < measuresLength; measureIndex++)
+                {
+                    measuresSum += draftIntCounters[vectorIndex, measureIndex];
+                }
+
+                draftAngle += (double)measuresSum / intCounter;
+            }
+
+            draftIntValue = (int)(int.MaxValue * draftAngle / vectorsLength);
+
+            Console.WriteLine("{0}\t{1}\t{2}", intCounter, intValue, draftIntValue);
+        }
+        while (draftIntValue != intValue);
+
+        Console.ReadKey(intercept: true);
+    }
+}
